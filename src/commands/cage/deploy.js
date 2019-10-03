4bd254e9c81231b533fc78faafdbb28df5a37a16
Shane Curran <shane@evervault.com>
2019-10-03 14:51:34 +0100

    Added config.json to .gitignore

--- src/commands/cage/deploy.js
+++ src/commands/cage/deploy.js
@@ -1,5 +1,5 @@
-const {Command, flags} = require('@oclif/command')
-const config = require("../../../config")
+const {Command} = require('@oclif/command')
+const config = require('../../../config')
 const request = require('request')
 const fs = require('fs')
 const logSymbols = require('log-symbols')
@@ -22,14 +22,14 @@ class DeployCommand extends Command {
         cli.action.stop(logSymbols.success)
         cli.action.start('Submitting request to evervault API')
 				request({
-			    url: config.api.base_url + "/cages",
-			    method: "POST",
+			    url: config.api.base_url + '/cages',
+			    method: 'POST',
 					headers: {
-						Authorization: "Bearer " + config.api.token
+						Authorization: 'Bearer ' + config.api.token
 					},
 			    json: {
 						metadata: {
-							framework: "nodejs",
+							framework: 'nodejs',
 							libraries: []
 						},
 						source: data.toString()
@@ -37,43 +37,38 @@ class DeployCommand extends Command {
 				}, function (err, head, body) {
 
 					if (err) throw err;
-          cli.action.stop(logSymbols.success)
+					cli.action.stop(logSymbols.success)
 
-          if (body.name) {
-            cli.action.start("Waiting for cage " + chalk.bold(body.name) + " to build")
+					if (body.name) {
+						cli.action.start('Waiting for cage ' + chalk.bold(body.name) + ' to build')
 
-            var checkBuild = setInterval(function () {
-              // check if the cage has been built yet
-              request({
-      			    url: config.api.base_url + "/cages/" + body.name,
-      			    method: "GET",
-      					headers: {
-      						Authorization: "Bearer " + config.api.token
-      					}
-      				}, function (err, head, body) {
-                body = JSON.parse(body)
-                if (body.status === "deployed") {
-                  cli.action.stop(logSymbols.success);
-                  console.log("Cage successfully deployed! " + logSymbols.success)
-                  clearInterval(checkBuild);
-                  console.log()
-                  console.log("You can access your new cage at " + body.url)
-                }
-              });
-            }, 1000);
-          } else {
-            console.error("There was an error submitting your cage")
-          }
-					// if (body.hash) {
-					// 	console.log("Your cage has successfully been deployed. Access it at https://e/" + body._id);
-					// } else {
-					// 	console.error("There was an error deploying your cage")
-					// }
-				})
-			} else {
-				console.error("Please login using `evervault login` before deploying a cage");
-			}
-		});
+						var checkBuild = setInterval(function () {
+							// check if the cage has been built yet
+							request({
+									url: config.api.base_url + '/cages/' + body.name,
+									method: 'GET',
+										headers: {
+											Authorization: 'Bearer ' + config.api.token
+										}
+									}, function (err, head, body) {
+								body = JSON.parse(body)
+								if (body.status === 'deployed') {
+								cli.action.stop(logSymbols.success);
+								console.log('Cage successfully deployed! ' + logSymbols.success)
+								clearInterval(checkBuild);
+								console.log()
+								console.log('You can access your new cage at ' + body.url)
+								}
+							});
+						}, 1000);
+				} else {
+					console.error('There was an error submitting your cage')
+				}
+			})
+		} else {
+			console.error('Please login using `evervault login` before deploying a cage');
+		}
+	});
   }
 }
 
